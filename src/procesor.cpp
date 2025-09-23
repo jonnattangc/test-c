2546e9f825dbeea9509be3bf5efa0f34d1f378c7
Jonnattan Griffiths <jonnattan.griffiths@ionix.cl>
2025-09-23 09:03:54 -0300

    Procesador de archivos mas veloz

--- src/procesor.cpp
+++ src/procesor.cpp
@@ -4,10 +4,10 @@
 #include <dirent.h>
 
 #define LEN_LINE 1024
-#define LEN_FILE_SIZE 800 * 1024 * 1024 // en MB
+#define LEN_FILE_SIZE 800000000 // en MB
 
-bool search(char *pCode, char *pDate, char *pFolder1, char *pFolder2, char *pFolder3);
-bool analize(char *pCode, char *pDate, char *pFolder);
+bool analize(const char *pCode, const char *pDate, const char *pFolder);
+bool search(const char *pCode, const char *pDate, const char *pFolder1, const char *pFolder2, const char *pFolder3);
 
 int main(int argc, char *argv[])
 {
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     char *pNamefile = NULL;
     char *pFolder1 = NULL, *pFolder2 = NULL, *pFolder3 = NULL;
     FILE *pFile = NULL, *pOutFile = NULL, *pNoFoundFile = NULL;
-    
+
     printf("Iniciando...\n");
     if( argc > 2 ) {
       pNamefile = (char *) &argv[1][0];
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
         pFolder2 = (char *) &argv[3][0];
       if( argc > 4 )
         pFolder3 = (char *) &argv[4][0];
-      
+
       printf("Nombre de archivo a procesar: %s\n", pNamefile );
       printf("Carpeta 1: %s Carpeta 2: %s Carpeta 3: %s\n", pFolder1, pFolder2!=NULL?pFolder2:"Nada", pFolder3!=NULL?pFolder3:"Nada" );
     } else {
       printf("Debe indicar el nombre del archivo a procesar y la(s) carpeta de logs de entrada\n");
       exit(EXIT_FAILURE);
-    } 
+    }
 
     pFile = fopen(pNamefile, "r");
     if (pFile == NULL){
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    int ret = fseek (pFile, 0, SEEK_END); 
+    int ret = fseek (pFile, 0, SEEK_END);
     if (ret != 0) {
       printf("Error: fseek retorno %d\n", ret);
       exit(EXIT_FAILURE);
@@ -79,12 +79,15 @@ int main(int argc, char *argv[])
       monto = 0.0f;
       number = sscanf(line, "%[^,],%[^,],%f,%s", mccode, code, &monto, date);
       if( number == 4 ) {
-        i++;
         if(search(code, date, pFolder1, pFolder2, pFolder3)){
           fprintf(pOutFile, "%s\n", line);
         }else{
+          printf("%s no encontrado en archivos\n", code);
           fprintf(pNoFoundFile, "%s, %s\n", date, code);
         }
+        fflush(pOutFile);
+        fflush(pNoFoundFile);
+        i++;
       }
       memset( line, 0, LEN_LINE );
     }
@@ -97,9 +100,10 @@ int main(int argc, char *argv[])
 }
 
 
-bool search(char *pCode, char *pDate, char *pFolder1, char *pFolder2, char *pFolder3){
-    // printf("Buscando %s en fecha %s\n", pCode, pDate );
-    if( analize(pCode, pDate, pFolder1) ) return true;
+bool search(const char *pCode, const char *pDate, const char *pFolder1, const char *pFolder2, const char *pFolder3){
+    // printf("Buscando [%s] en fecha [%s] \n", pCode, pDate );
+    bool finded = analize(pCode, pDate, pFolder1);
+    if( finded ) return true;
     if( pFolder2 != NULL ) {
       if( analize(pCode, pDate, pFolder2) ) return true;
     }
@@ -109,51 +113,60 @@ bool search(char *pCode, char *pDate, char *pFolder1, char *pFolder2, char *pFol
     return false;
 }
 
-bool analize(char *pCode, char *pDate, char *pFolder) {
-  DIR *pDir;
-  FILE *pFile = NULL;
+bool analize( const char *pCode, const char *pDate, const char *pFolder ) {
+  // printf("Buscando en [%s]\n", pFolder );
+  DIR *pDir = NULL;
   char fileName[LEN_LINE];
-  struct dirent *pEntry;  
+  char *pBuffer = new char[LEN_FILE_SIZE];
+  struct dirent *pEntry = NULL;
   bool finded = false;
+
   if (pFolder != NULL ) {
-      pDir = opendir(pFolder);
-      if (pDir == NULL) {
-        printf("Error: No se pudo abrir la carpeta %s\n", pFolder );
-        return false;
-      }
-      while ((pEntry = readdir(pDir)) != NULL) {
-        if( pEntry->d_type == DT_REG ) {
-          if( strstr(pEntry->d_name, pDate) != NULL ) {
-            memset( fileName, 0, LEN_LINE );
-            memcpy( fileName, pFolder, strlen(pFolder) );
-            memcpy( &fileName[strlen(pFolder)], pEntry->d_name, strlen(pEntry->d_name) );
-            // printf("Archivo: %s Se considera\n", pfileName);
-            pFile = fopen(fileName, "rb");
-            if (pFile == NULL){
-                printf("Error: No se pudo abrir el archivo %s\n", fileName);
-                continue;
-            }
-            char buffer[LEN_FILE_SIZE];
-            memset( buffer, 0, LEN_FILE_SIZE );
-            size_t result = fread(buffer, 1, LEN_FILE_SIZE, pFile);
-            if (result <= 0) {
-              printf( "ERROR: Leyendo archivo %s\n", pEntry->d_name );
-              fclose(pFile);
-              continue;
-            }
-            // no se considera may'usculas o min'usculas
-            char *pValue = strstr(buffer, pCode);
-            if( pValue != NULL ) {
-              printf("%s encontrado en %s\n", pCode, fileName );
-              finded = true;
-            }
-            fclose(pFile);
-          }
-        }
-        if( finded ) break;
-      }
-      // Cerrar el directorio
-      closedir(pDir);
+    pDir = opendir(pFolder);
+    if (pDir == NULL) {
+      printf("Error: No se pudo abrir la carpeta %s\n", pFolder );
+      return finded;
     }
+    while ((pEntry = readdir(pDir)) != NULL) {
+      // printf("Abro archivos del directorio [%d]\n", pEntry->d_type);
+      if( pEntry->d_type == DT_REG ) {
+        //printf("Name Date [%s] [%s] ", pDate, pEntry->d_name);
+        char *pResp = strstr(pEntry->d_name, pDate);
+        char *pGz = strstr(pEntry->d_name, ".gz");
+        //printf("Resp [%s]\n", pResp!=NULL?"OK":"NULL");
+        if( pResp != NULL && pGz == NULL) {
+          // printf("Analizo Archivo: %s\n", pEntry->d_name);
+          memset( fileName, 0, LEN_LINE );
+          memcpy( fileName, pFolder, strlen(pFolder) );
+          memcpy( &fileName[strlen(pFolder)], pEntry->d_name, strlen(pEntry->d_name) );
+          // printf("Archivo: %s se analizara\n", fileName);
+          FILE *pFile = fopen(fileName, "r");
+          if (pFile == NULL) {
+              printf("Error: No se pudo abrir el archivo %s\n", fileName);
+              continue;
+          }
+          memset( pBuffer, 0, LEN_FILE_SIZE );
+          size_t result = fread(pBuffer, 1, LEN_FILE_SIZE, pFile);
+          if (result <= 0) {
+            printf( "ERROR: Leyendo archivo %s\n", pEntry->d_name );
+            fclose(pFile);
+            continue;
+          }
+          // no se considera may'usculas o min'usculas
+          char *pValue = strstr(pBuffer, pCode);
+          if( pValue != NULL ) {
+            printf("%s encontrado en %s\n", pCode, fileName );
+            finded = true;
+          }
+          fclose(pFile);
+
+        }
+      }
+      if( finded ) break;
+    }
+    // Cerrar el directorio
+    closedir(pDir);
+  }
+  delete [] pBuffer;
   return finded;
 }
